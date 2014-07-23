6a16a85400a578dbe682ef0876fa62034b04d204
bartek 'basz' szurgot <git@baszerr.eu>
2014-07-23 21:03:39 +0200

    typedefs moved to a more aproperiate location

--- Index.hpp
+++ Index.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <memory>
 #include "Impl/types.hpp"
 
 struct Index
@@ -13,3 +14,7 @@ struct Index
   virtual Impl::FilesList filesContainingWord(std::string const& word) = 0;
   virtual Impl::IndexingStatus addToIndex(std::string fileName) = 0;
 };
+
+
+using IndexPtr   = std::unique_ptr<Index>;
+using IndexShPtr = std::shared_ptr<Index>;
--- buildIndex.hpp
+++ buildIndex.hpp
@@ -1,8 +1,4 @@
 #pragma once
-#include <memory>
 #include "Index.hpp"
-
-using IndexPtr   = std::unique_ptr<Index>;
-using IndexShPtr = std::shared_ptr<Index>;
 
 IndexPtr buildIndex();
