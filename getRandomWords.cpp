a61c8a3c0ab127bd3a4e03537ec7cb6e7f5a6048
bartek 'basz' szurgot <git@baszerr.eu>
2014-07-23 21:02:54 +0200

    function helper template to optain random words from given list of files

---
+++ getRandomWords.cpp
@@ -0,0 +1,7 @@
+#include "getRandomWords.hpp"
+
+std::vector<std::string> getRandomWords(std::vector<std::string> const& files, unsigned seed)
+{
+  // TODO
+  return {};
+}
---
+++ getRandomWords.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> getRandomWords(std::vector<std::string> const& files, unsigned seed);
