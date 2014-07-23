3c6517503cc5c3b09e1f125ee2fcc78964f60ede
bartek 'basz' szurgot <git@baszerr.eu>
2014-07-23 22:21:37 +0200

    example MyIndex implementation - as crapy as possible ;)

--- Impl/MyIndex.cpp
+++ Impl/MyIndex.cpp
@@ -1,3 +1,6 @@
+#include <set>
+#include <vector>
+#include <fstream>
 #include "MyIndex.hpp"
 
 namespace Impl
@@ -5,14 +8,40 @@ namespace Impl
 
 FilesList MyIndex::filesContainingWord(std::string const& word)
 {
-  // TODO
-  return {};
+  std::lock_guard<std::mutex> guard{mutex_};
+  std::set<std::string>       uniqueFiles;
+  for(auto const& e: index_)
+    if(e.first==word)
+      uniqueFiles.insert(e.second);
+  return FilesList{ begin(uniqueFiles), end(uniqueFiles) };
+}
+
+
+namespace
+{
+auto getWordsFromFile(std::string const& file)
+{
+  std::ifstream infile;
+  infile.open(file);
+  if(not infile.is_open())
+    throw std::runtime_error{"unable to open file: " + file};
+
+  std::vector<std::string> words;
+  std::string              line;
+  while( std::getline(infile, line) )
+    words.push_back(line);
+
+  return words;
+}
 }
 
 IndexingStatus MyIndex::addToIndex(std::string fileName)
 {
-  // TODO
-  return {};
+  std::lock_guard<std::mutex> guard{mutex_};
+  const auto words = getWordsFromFile(fileName);
+  for(auto const& w: words)
+    index_.push_back( Entry{w, fileName} );
+  return true;
 }
 
 }
--- Impl/MyIndex.hpp
+++ Impl/MyIndex.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <list>
+#include <mutex>
 #include "Index.hpp"
 
 namespace Impl
@@ -9,6 +11,11 @@ struct MyIndex final: public Index
 {
   virtual FilesList filesContainingWord(std::string const& word) override;
   virtual IndexingStatus addToIndex(std::string fileName) override;
+
+private:
+  using Entry = std::pair<std::string, std::string>;    // word + file
+  std::mutex       mutex_;
+  std::list<Entry> index_;
 };
 
 }
