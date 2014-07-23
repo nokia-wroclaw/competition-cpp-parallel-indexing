a7f9a2a0e20f513ad73b15e9920331916e567292
bartek 'basz' szurgot <git@baszerr.eu>
2014-07-23 21:04:39 +0200

    benchmark files implemenation in progress

--- benchmark.cpp
+++ benchmark.cpp
@@ -1,15 +1,57 @@
-#include <memory>
 #include <vector>
+#include <iostream>
+#include <typeinfo>
+#include <stdexcept>
+
 #include "Index.hpp"
 #include "buildIndex.hpp"
 #include "Reader.hpp"
+#include "getRandomWords.hpp"
 
 constexpr unsigned writersCount = 2;
 constexpr unsigned readersCount = 10;
 
-int main()
+auto filesList(int argc, char** argv)
 {
-  IndexShPtr index = buildIndex();
+  std::vector<std::string> files;
+  for(int i=1; i<argc; ++i)
+    files.emplace_back(argv[i]);
+  return files;
+}
 
-  std::vector<Reader> readers;
+
+template<typename ...Args>
+auto makeReaders(Args... args)
+{
+  std::vector<ReaderPtr> readers;
+  for(unsigned i=0; i<readersCount; ++i)
+    readers.push_back( std::make_unique<Reader>(args..., i) );
+  return readers;
+}
+
+
+int main(int argc, char** argv)
+{
+  try
+  {
+    if(argc<1+1)
+    {
+      std::cerr << argv[0] << " <file1> <file2> ... <fileN>" << std::endl;
+      return 1;
+    }
+
+    const auto files   = filesList(argc, argv);
+    const auto words   = std::make_shared<std::vector<std::string>>( getRandomWords(files, argc) );
+    IndexShPtr index   = buildIndex();
+    const auto readers = makeReaders(index, words);
+
+    // TODO: add writers
+    // TODO: add timeout
+    // TODO: add results collecting
+  }
+  catch(std::exception const& ex)
+  {
+    std::cerr << "EXCEPTION: " << typeid(ex).name() << ": " << ex.what() << std::endl;
+    return 2;
+  }
 }
